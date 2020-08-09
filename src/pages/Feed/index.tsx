27e0eda0b212e69742830b431016a8d3172a1954
haple <aleph.oliveira@etec.sp.gov.br>
2020-08-09 20:13:25 -0300

    Search users by username on new post

--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -4,6 +4,10 @@ import { useHistory } from 'react-router-dom';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import { FiSend } from 'react-icons/fi';
+import ReactTextareaAutocomplete from '@webscopeio/react-textarea-autocomplete';
+import '@webscopeio/react-textarea-autocomplete/style.css';
+import defaultAvatar from '~/assets/default-avatar.png';
+
 import Header from '~/components/Header';
 
 import {
@@ -13,6 +17,7 @@ import {
   Post,
   NewPost,
   Ranking,
+  UserItem,
   CatalogCallToAction,
 } from './styles';
 import api from '~/services/api';
@@ -26,6 +31,26 @@ interface IUserData {
 interface IRemainingPointsToSend {
   remaining_points: number;
 }
+
+interface IUserSearchResult {
+  id: string;
+  name: string;
+  avatar: string;
+  username: string;
+}
+
+const UserListItem = ({ entity }: { entity: IUserSearchResult }) => (
+  <UserItem>
+    <div>
+      <img
+        src={entity.avatar ? entity.avatar : defaultAvatar}
+        alt={entity.username}
+      />
+      <br />
+      <span>{entity.name}</span>
+    </div>
+  </UserItem>
+);
 
 const Feed: React.FC = () => {
   const history = useHistory();
@@ -57,6 +82,15 @@ const Feed: React.FC = () => {
     loadRemainingPointsToSend();
   }, []);
 
+  const searchUsersByUsername = useCallback(async (username: string) => {
+    const response = await api.get<IUserSearchResult[]>('/users', {
+      params: {
+        username_like: username,
+      },
+    });
+    return response.data;
+  }, []);
+
   return (
     <>
       <Header />
@@ -75,18 +109,35 @@ const Feed: React.FC = () => {
             </div>
 
             <Form ref={formRef} onSubmit={handleSubmit}>
-              <textarea
-                name="new_post_content"
-                id=""
-                cols={30}
-                rows={10}
-                placeholder="Que tal reconhecer aquela pessoa bacana que trabalha com você?"
-              />
+              <div>
+                <ReactTextareaAutocomplete
+                  name="new_post_content"
+                  cols={70}
+                  rows={10}
+                  placeholder="Que tal reconhecer aquela pessoa bacana que trabalha com você?"
+                  className="my-textarea"
+                  // onChange={(e) => console.log(e.target.value)}
+                  loadingComponent={() => <span>Loading</span>}
+                  trigger={{
+                    '@': {
+                      dataProvider: (token) => {
+                        return searchUsersByUsername(token);
+                      },
+                      component: UserListItem,
+                      output: (item, trigger) => `@${item.username}`,
+                      allowWhitespace: false,
+                      afterWhitespace: true,
+                    },
+                  }}
+                />
+              </div>
 
-              <Button light type="submit">
-                <FiSend />
-                Enviar
-              </Button>
+              <div>
+                <Button light type="submit">
+                  <FiSend />
+                  Enviar
+                </Button>
+              </div>
             </Form>
           </NewPost>
         </Content>
--- src/pages/Feed/styles.ts
+++ src/pages/Feed/styles.ts
@@ -2,9 +2,15 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   /* background: #5c5c; */
-  margin: 20px 100px;
+  margin: 20px 160px;
   display: flex;
   justify-content: space-between;
+
+  @media screen and (max-width: 700px) {
+    aside {
+      display: none;
+    }
+  }
 `;
 
 export const Content = styled.div`
@@ -13,6 +19,8 @@ export const Content = styled.div`
   /* flex-wrap: wrap; */
   display: flex;
   flex-direction: column;
+  width: 650px;
+  margin-right: 20px;
 
   h2 {
     font-size: 24px;
@@ -33,7 +41,8 @@ export const NewPost = styled.div`
   border: 2px solid #c5ced6;
   background: #fff;
   padding: 20px;
-  div {
+  width: auto;
+  & > div {
     display: flex;
     flex-direction: row;
     margin-bottom: 5px;
@@ -42,15 +51,36 @@ export const NewPost = styled.div`
       font-weight: normal;
       margin-top: 0;
       margin-right: 5px;
+      width: auto;
     }
   }
 
   textarea {
+    width: 100%;
+    min-height: 100px;
+    min-width: 100%;
+    max-width: 100%;
     border: 2px solid #c5ced6;
-    border-radius: 2%;
+    border-radius: 5px;
+    padding: 10px;
+    color: #293845;
+    font-family: 'Roboto', sans-serif;
+    font-size: 14px;
+  }
+  form {
+    div {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+    }
+    button {
+      /* width: 150px;
+      height: 50px; */
+    }
   }
 `;
 export const Ranking = styled.div``;
+
 export const CatalogCallToAction = styled.div`
   border: 2px solid #c5ced6;
   background: #fff;
@@ -75,5 +105,21 @@ export const CatalogCallToAction = styled.div`
     font-weight: normal;
     height: 50px;
     width: auto;
+  }
+`;
+
+export const UserItem = styled.div`
+  color: #293845;
+  font-family: 'Roboto', sans-serif;
+  font-size: 14px;
+  div {
+    display: flex;
+    flex-direction: row;
+  }
+
+  img {
+    border-radius: 50%;
+    margin-right: 10px;
+    height: 35px;
   }
 `;
